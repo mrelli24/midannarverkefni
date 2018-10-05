7767027618fadee5c0f32b87ddd85ebe8dc60504
mrelli24 <32517753+mrelli24@users.noreply.github.com>
2018-10-05 17:31:17 +0000

    Add files via upload

---
+++ M.py
@@ -0,0 +1,35 @@
+from sys import argv
+from bottle import *
+
+import urllib.request,json
+
+with urllib.request.urlopen("http://apis.is/petrol/")as url:
+    data = json.loads(url.read().decode())
+
+@route('/static/<skra>')
+def static_skra(skra):
+    return static_file(skra, root='./static/')
+
+@route("/")
+def index():
+    return template("index.tpl", data=data)
+
+@route('/company/<company>')
+def index(company):
+    return template('company',data=data, c=company)
+
+
+@route('/moreinfo/<key>')
+def index(key):
+    return template('moreinfo',data=data,k=key)
+
+
+########################################################################
+
+@error(404)
+def villa(error):
+    return "<h2 style='color:red'>Þessi síða finnst ekki</h2>"
+
+
+run(host='0.0.0.0', port=os.environ.get('PORT'))
+#run(host="localhost",port=8080,debug=True)
